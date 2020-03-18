910a9064be3baad394f6e401936326b25768bd7e
apple <apple@apples-MacBook-Pro-2.local>
2020-03-18 16:42:55 -0700

    working on router

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 // router
-const carsRouter = require('./data/carsRouter.js');
+const carsRouter = require('../data/carsRouter.js');
 
 const server = express();
 
@@ -11,5 +11,10 @@ server.use(helmet());
 server.use(express.json());
 
 server.use('/api/cars', carsRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up and running' })
+})
+
 
 module.exports = server;
--- data/carsRouter.js
+++ data/carsRouter.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const knex = require('knex');
 
+const router = express.Router();
+
+const knexConfig = {
+    client: 'sqlite3', //database driver
+    connection: { // could be an object or string
+        filename: './data/cars.db3'
+    },
+    useNullAsDefault: true, // only needed for SQLite3
+}
+
+// db represents a live connection to the database
+const db = knex(knexConfig);
+
+
+module.exports router;
